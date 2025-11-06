209b763b156e5f42a351e6cdb64b3dbb3bfdd903
Masalale <c.chomba@alustudent.com>
2025-11-06 14:16:52 +0200

    feat: add keymap to open LazyVim dashboard with <Space>;

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open the LazyVim dashboard with <Space>;
+vim.keymap.set("n", "<Space>;", function()
+  require("snacks").dashboard()
+end, { desc = "Open LazyVim Dashboard" })
